ee1c821862a68a8a13024b51ddbacbef23ba3cc8
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2022-12-08 10:06:26 +0000

    doc(client): Improve example

--- examples/existing-cloudtrail-s3-notifications-with-filter/main.tf
+++ examples/existing-cloudtrail-s3-notifications-with-filter/main.tf
@@ -18,21 +18,18 @@ module "aws_cloudtrail" {
   bucket_arn              = "arn:aws:s3:::lacework-ct-bucket-8805c0bf"
 
   # Use S3 Bucket Notifications
-  use_s3_bucket_notification = true
-  s3_notification_type       = "SNS"
+  use_s3_bucket_notification    = true
+  s3_notification_type          = "SNS"
+  sns_topic_filter_policy_scope = "MessageBody"
   sns_topic_filter_policy = jsonencode({
     # SNS filtering https://docs.aws.amazon.com/sns/latest/dg/sns-message-filtering.html
     # SNS event example https://docs.aws.amazon.com/awscloudtrail/latest/userguide/configure-sns-notifications-for-cloudtrail.html
     # S3 cloudtrail path format https://docs.aws.amazon.com/AmazonS3/latest/userguide/cloudtrail-request-identification.html
-    s3ObjectKey = {
-      Type = "String.Array"
-      Value = [
-        for accountid in local.accountids_with_lacework_enabled :
-        {
-          prefix = "AWSLogs/${accountid}/"
-        }
-      ]
-    }
+    s3ObjectKey = [
+      for accountid in local.accountids_with_lacework_enabled :
+      {
+        prefix = "AWSLogs/${accountid}/"
+      }
+    ]
   })
-  sns_topic_filter_policy_scope = "MessageBody"
 }
